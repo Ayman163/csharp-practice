8db712cd15bf9a9c86f729607862711244bc760a
Ayman <aymanss2063@gmail.com>
2026-07-08 23:19:30 -0700

    Create Upper-and-Lower program for string case conversion
    This program converts a string to uppercase and then to lowercase, displaying both results.

---
+++ New/Upper-and-Lower.cs
@@ -0,0 +1,17 @@
+using system
+
+namespace My FIrst Program
+{
+  class Program
+  {
+  
+    static void Main(string[] args)
+    {
+      String fullname = "Ayman pro";
+      fullname = fullname.ToUpper();
+      Console.WriteLine(fullname)
+      fullname = fullname.ToLower();
+      Console.WriteLine(fullname)
+    }
+  }
+}
