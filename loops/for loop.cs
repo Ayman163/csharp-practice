61904b3a8e0cadf3319675f77cf365adf8935966
Ayman <aymanss2063@gmail.com>
2026-07-21 12:53:49 +0300

    Add for loop to print numbers from 1 to 10

---
+++ loops/for loop.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace MyFirstApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         for(int i =1; i <11;i++)
+            {
+                Console.WriteLine(i);
+            }  
+        }
+    }
+}
