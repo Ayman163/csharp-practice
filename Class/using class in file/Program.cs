ebd9ccc26080d4ed350e6f1a8549f89089d15c7f
Ayman <aymanss2063@gmail.com>
2026-07-28 14:58:59 +0300

    Create Program.cs with main method and message calls
    Implement main program structure and call message methods.

---
+++ Class/using class in file/Program.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace MyFirstProgram
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // class = A bundle of related code.
+            //              Can be used as a blueprint to create objects (OOP)
+            
+            Messages.Hello();
+            Messages.Waiting();
+            Messages.Bye();
+
+            Console.ReadKey();
+        }
+    }
+}
